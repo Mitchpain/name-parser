db646f3dad99b5113d59512c8b5838f560e86bbe
michpoud <michpoud@gmail.com>
2020-12-31 10:57:43 -0500

    create movie folder

--- parseTorrentName.ts
+++ parseTorrentName.ts
@@ -1,11 +1,36 @@
 const ptt = require("parse-torrent-title");
+const fs = require('fs')
 const minimist = require('minimist');
+
+interface movieInformation {
+    title:string,
+    year:string
+}
+
+const handleMovie = (information: movieInformation, targetDir:string) => {
+    const movieTitle = information.title
+    const year = information.year ? information.year : ""
+    const folderName = `${movieTitle} (${year})`
+    const dir = `${targetDir}/Movies/${folderName}`
+   if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
+}
+
+
 let args = minimist(process.argv.slice(2))
 const torrentName = args.n
+const targetDir = args.t
 const information = ptt.parse(torrentName);
 
 const torrentIsMovie = information.episode === undefined
- 
+
+if (torrentIsMovie){
+    handleMovie(information, targetDir)
+}
+
+
+
 console.log(information.title);      // Game of Thrones
 console.log(information.season);     // 1
 console.log(information.episode);    // 1
--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+#TORRENT_NAME: Torrent Name
+#OUTPUT_PATH: Media center target
 TORRENT_NAME="Funny Games (2007) [BluRay] [1080p] [YTS.AM]"
-node parseTorrentName.js -n "$TORRENT_NAME"
+#TORRENT_PATH=""
+OUTPUT_PATH="$HOME/Media"
+npm run build
+node parseTorrentName.js -n "$TORRENT_NAME" -t "$OUTPUT_PATH"
